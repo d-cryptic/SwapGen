cb2d31fc2e769764d57424960ba923a13e12b6b8
Barun Debnath <barundebnath91@gmail.com>
2022-02-13 02:44:07 +0530

    alan ai done

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,21 @@ import Image from "next/image";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Tab from "../components/Tab";
+// import alanBtn from "@alan-ai/alan-sdk-web";
+import dynamic from "next/dynamic";
 
 export default function Home() {
-  // useEffect(() => {
-  //   alanBtn({
-  //     key: "ac1cdcabf16131c9aa1704feef0ff62a2e956eca572e1d8b807a3e2338fdd0dc/stage",
-  //     onCommand: (commandData) => {
-  //       if (commandData.command === "go:back") {
-  //         // Call the client code that will react to the received command
-  //       }
-  //     },
-  //   });
-  // }, []);
+  useEffect(() => {
+    const alanBtn = require("@alan-ai/alan-sdk-web");
+    alanBtn({
+      key: "ac1cdcabf16131c9aa1704feef0ff62a2e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: (commandData) => {
+        if (commandData.command === "go:back") {
+          // Call the client code that will react to the received command
+        }
+      },
+    });
+  }, []);
 
   return (
     <div>
