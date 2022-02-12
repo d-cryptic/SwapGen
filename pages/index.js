a3d44686f1f17b4c7440c91be31e86a9dfdddc18
Barun Debnath <barundebnath91@gmail.com>
2022-02-13 04:13:26 +0530

    all done

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
             content="initial-scale=1.0, width=device-width"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;500;700;900&display=swap"
             rel="stylesheet"></link>
